c51abdc0ab65ace77b970e4b3b4b2438799437c1
Onogi <onogiakio@gmail.com>
2019-05-31 09:58:05 +0900

    Add files via upload

---
+++ retrieve.py
@@ -0,0 +1,6 @@
+import DNN3
+
+datano=11
+modelno=1
+DNN3.RetrieveMLP(DataNo=datano, SimNo_start=0, SimNo_end=3, ModelNo=modelno)
+DNN3.RetrieveWeights(DataNo=datano, SimNo_start=0, SimNo_end=3, ModelNo=modelno)
